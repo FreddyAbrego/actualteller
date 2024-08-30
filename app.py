ac964d8c953410f3a1f5f48dfefc3797cf4a9233
freddyabrego <dev@freddyabrego.com>
2024-08-30 10:29:26 -0400

    Update to docker-compose

--- app.py
+++ app.py
@@ -263,6 +263,6 @@ def get_db():
 # calls main()
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(debug=True, port=8001, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')
 
     
